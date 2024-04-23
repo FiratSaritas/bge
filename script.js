49de9edebf524298347f36fbbe0ea9ff6149132d
Firat Saritas <firat.saritas@emilfrey.ch>
2024-04-23 12:32:23 +0200

    upgrade

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
     $(window).on('scroll', function() {
-        var scrollPosition = $(this).scrollTop();
-        var maxScroll = $(document).height() - $(window).height();
+        var windowHeight = $(window).height();
+        var documentHeight = $(document).height();
+        var scrollTop = $(window).scrollTop();
+        
+        // Calculate the percentage scrolled
+        var scrollPercentage = (scrollTop / (documentHeight - windowHeight)) * 100;
+        
+        // Calculate the year based on the scroll percentage
         var minYear = 1875;
         var maxYear = 2024;
-
-        // Ensure scrollPosition doesn't exceed maxScroll
-        scrollPosition = Math.min(scrollPosition, maxScroll);
-
-        // Calculate the year based on the scroll position
-        var currentYear = Math.round((maxYear - minYear) * (scrollPosition / maxScroll)) + minYear;
+        var currentYear = Math.round(((maxYear - minYear) * scrollPercentage) / 100) + minYear;
 
         // Calculate the corresponding counter value
         var counterValue = 150 - (2024 - currentYear);
-
+        
         // Update the year and counter displayed
         $('#year').text(currentYear);
         $('#counter').text(counterValue);
